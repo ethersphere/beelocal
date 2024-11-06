50d5f7b51a9ba68577ea3b4ee8b2d2285bbb635d
Ljubisa <ljubisa.rs@gmail.com>
2024-11-06 18:33:53 +0100

    chore: use build docker image from makefile for dev env

--- beelocal.sh
+++ beelocal.sh
@@ -217,7 +217,7 @@ build() {
                 --cache-from type=gha,ref=k3d-registry.localhost:5000/ethersphere/bee .
         fi
     else
-        docker build -t k3d-registry.localhost:5000/ethersphere/bee:"${IMAGE_TAG}" .
+        make docker-build IMAGE=k3d-registry.localhost:5000/ethersphere/bee:"${IMAGE_TAG}"
     fi
     if [[ -z $SKIP_PUSH ]]; then
         docker push k3d-registry.localhost:5000/ethersphere/bee:"${IMAGE_TAG}"
